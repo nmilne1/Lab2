147ac6aafc1a9a5fc3929b2acf349d03d558298a
nmilne1 <54378904+nmilne1@users.noreply.github.com>
2019-08-26 16:05:06 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,4 +15,21 @@
         hello2Function(name2);
         return 0;
     }
+//
+/// \f hello1Function
+/*
+ * hello1Function.cpp
+ *
+ */
+#include <iostream>
+using namespace std;
+int hello1Function(string name1)
+{
+    cout << "hello World " << name1 << endl;
+    return 0;
+}
+
+
+///hello1Function
+
 
